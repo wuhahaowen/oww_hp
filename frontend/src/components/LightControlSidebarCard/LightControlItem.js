41511341f5936124e49ec3f2196f37934e278184
oww <732894893@163.com>
2025-10-22 14:13:30 +0800

    #还原线上 1.4.1

--- frontend/src/components/LightControlSidebarCard/LightControlItem.js
+++
@@ -1,9 +0,0 @@
-
-export  const  renderText =   (componentMode,clickType) => {
-    if(componentMode === 'lightControl' && clickType ){
-        return "ON"
-    }else  if(componentMode === 'lightControl' && clickType ){
-        return "OFF"
-    }
-    return "OFF"
-}
